5b9bab46976633bb2d296f1ba1a828f429a023ab
zhaozixu <163083654+bitkira@users.noreply.github.com>
2025-03-31 22:45:44 +0800

    changed

--- exercise/ice-webdevbasics3-main/starter/index.js
+++ exercise/ice-webdevbasics3-main/starter/index.js
@@ -1,8 +1,9 @@
 // This is where your JS goes!
+import { CS571_ID } from '../../../config.js';
 
-fetch('https://cs571api.cs.wisc.edu/rest/s25/ice/chili', {
+fetch('https://cs571.org/rest/s25/ice/chili', {
     headers: {
-        "X-CS571-ID": CS571.getBadgerId() // You may hardcode your Badger ID instead.
+        "X-CS571-ID": CS571_ID // You may hardcode your Badger ID instead.
     }
 })
 .then(res => {
@@ -14,7 +15,15 @@ fetch('https://cs571api.cs.wisc.edu/rest/s25/ice/chili', {
     }
 })
 .then(data => {
-    console.log(data);
+    console.log(data)
+
+    console.log("The 5 star")
+
+    let review = data.reviews
+    console.log(review.filter(r => r.rating === 5).map(r => r.txt))
+
+    console.log(data.recipe.map(init => init.split(":")))
+    
 })
 .catch(err => {
     alert("Uh oh! Something went wrong. Are you logged in with your Badger ID?")
