0192b00daf56fe0d8328491cc72c6fb52b1f6516
zhaozixu <163083654+bitkira@users.noreply.github.com>
2025-04-04 10:41:59 +0800

    hw2

--- homework/hw2-main/badger-book/app.js
+++ homework/hw2-main/badger-book/app.js
@@ -16,9 +16,14 @@ fetch("https://cs571.org/rest/s25/hw2/students", {
 })
 .then(res => res.json())
 .then(StuentData => 
-	{	document.getElementById("num-results").innerText = StuentData.length
+	{	document.getElementById("students").innerHTML = ""
+		StuentData = StuentData.filter( data => (data.name.first + data.name.last).toLowerCase().includes(document.getElementById("search-name").value.trim().toLowerCase()) && data.major.toLowerCase().includes(document.getElementById("search-major").value.trim().toLowerCase()) && data.interests.some(interest => interest.toLowerCase().includes(document.getElementById("search-interest").value.trim().toLowerCase())))
+		document.getElementById("num-results").innerText = StuentData.length
+		const studentsContainer = document.createElement("div");
+        studentsContainer.className = "row row-cols-1 row-cols-sm-1 row-cols-md-2 row-cols-lg-3 row-cols-xl-4";
 		for (student of StuentData){
 			const div = document.createElement("div")
+			div.className = "col mb-4"
 			const name = document.createElement("h1")
 			const major = document.createElement("p")
 			const intro = document.createElement("p")
@@ -30,18 +35,19 @@ fetch("https://cs571.org/rest/s25/hw2/students", {
 				interests.appendChild(interest)
 			}
 			name.innerText = student.name.first + " " + student.name.last
-			intro.innerText = student.name.first+" is taking "+ student.numCredits+" and is"+(student.fromWisconsin?"":"NOT")+" from Wisconsin"
+			intro.innerText = student.name.first+" is taking "+ student.numCredits+" credits and is"+(student.fromWisconsin?"":" NOT")+" from Wisconsin."
 			major.innerText = student.major
+			major.style.fontWeight = 'bold'
 			numinterest.innerText = "they have " + student.interests.length + " interests including...."
 			div.appendChild(name)
 			div.appendChild(major)
 			div.appendChild(intro)
 			div.appendChild(numinterest)
 			div.appendChild(interests)
-			
-			document.getElementsByTagName("body")[0].appendChild(div)
-	}
+			studentsContainer.appendChild(div);
 
+	}
+	document.getElementById("students").appendChild(studentsContainer);
 
 
 	})
